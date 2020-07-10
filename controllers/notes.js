9d2cb1df75f7bbbc2ddf474badfd2bfb1045be47
jknack0 <jknack@mail.sfsu.edu>
2020-07-10 10:31:59 -0700

    when posting a note the userId is saved and the note is saved in the user document

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -18,6 +19,8 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if(!body.content) {
     return response.status(400).json({
       error: 'content missing'
@@ -28,9 +31,13 @@ notesRouter.post('/', async (request, response) => {
     content: body.content,
     important: body.important || false,
     date: body.date || new Date(),
+    user: user._id,
   })
 
   const savedNote = await newNote.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.json(savedNote)
 })
 
